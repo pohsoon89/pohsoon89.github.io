39f9cdfc8bd5cb79b410bc8f073e443e33f50918
Jia Jing <richmanxinbox@gmail.com>
2017-02-12 13:51:54 +0800

    try again

--- script.js
+++ script.js
@@ -4,33 +4,55 @@ $(document).ready(function() {
   //   headers: {'Authorization': 'apikey 29038bc005974c5b9e439ee5023466a0-us14'},
   // });
 
-  function register($form) {
-    $.ajax({
-        type: $form.attr('method'),
-        url: $form.attr('action'),
-        data: $form.serialize(),
-        dataType    : 'json',
-        contentType: "application/json; charset=utf-8",
-        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
-        success     : function(data) {
-            if (data.result != "success") {
-                // Something went wrong, do something to notify the user. maybe
-                alert(data.msg);
-            } else {
-                // It worked, carry on...
-                alert("success");
-            }
-        }
+  // function register($form) {
+  //   $.ajax({
+  //       type: $form.attr('method'),
+  //       url: $form.attr('action'),
+  //       data: $form.serialize(),
+  //       dataType    : 'json',
+  //       contentType: "application/json; charset=utf-8",
+  //       error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+  //       success     : function(data) {
+  //           if (data.result != "success") {
+  //               // Something went wrong, do something to notify the user. maybe
+  //               alert(data.msg);
+  //           } else {
+  //               // It worked, carry on...
+  //               alert("success");
+  //           }
+  //       }
+  //   });
+  // }
+
+  // var $form = $('.validate');
+
+  // $('form input[type="submit"]').bind('click', function ( event ) {
+  //     if ( event ) event.preventDefault();
+  //     register($form);
+  // });
+
+
+  $(".trial-pack-validate .subscribe-button").click(function() {
+    $('#mc-embedded-subscribe-form').submit(function(event) {
+      var formData = {
+      'mce-NAME' : $('#mce-NAME').val(),
+      'mce-EMAIL' : $('#mce-EMAIL').val(),
+      'mce-MMERGE3' : $('#mce-MMERGE3').val()
+      };
+
+      $.ajax({
+        type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url         : 'https://github.us14.list-manage.com/subscribe/post?u=3e394ca01b95d97f157c72e12&amp;id=7a595cd258', // the url where we want to POST
+        data        : formData, // our data object
+        dataType    : 'json', // what type of data do we expect back from the server
+        encode          : true
+        }).done(function(data) {
+        console.log(data);
+      });
+
+      event.preventDefault();
     });
-  }
-
-  var $form = $('.validate');
-
-  $('form input[type="submit"]').bind('click', function ( event ) {
-      if ( event ) event.preventDefault();
-      register($form);
   });
-
 
   $(".work-btn").click(function() {
     $('html, body').animate({
