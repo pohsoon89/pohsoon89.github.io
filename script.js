6bafe44d277ab1c2ddf6c033e0fe63e257ab9db6
Jia Jing <richmanxinbox@gmail.com>
2017-02-14 02:52:30 +0800

    ready to show

--- script.js
+++ script.js
@@ -1,37 +1,130 @@
 $(document).ready(function() {
 
-  $(".trial-pack-validate .subscribe-button").click(function() {
-    $('.trial-pack-validate').submit(function(event) {
-      var formData = {
-      'mce-NAME' : $('.trial-pack-validate #mce-NAME').val(),
-      'mce-EMAIL' : $('.trial-pack-validate #mce-EMAIL').val(),
-      'mce-MMERGE3' : $('.trial-pack-validate #mce-MMERGE3').val()
-      };
+  ajaxMailChimpForm($("#standard-subscribe-form"), $("#thank-you-title"), $(".indicates-required"));
+  ajaxMailChimpForm($("#trial-subscribe-form"), $("#thank-you-title"), $(".indicates-required"));
 
-      console.log(formData);
+  function isValidEmail($form) {
+      // If email is empty, show error message.
+      // contains just one @
+      var email = $form.find("input[type='email']").val();
+      if (!email || !email.length) {
+          return false;
+      } else if (email.indexOf("@") == -1) {
+          return false;
+      }
 
-      $.ajax({
-        type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-        url         : 'https://github.us14.list-manage.com/subscribe/post?u=3e394ca01b95d97f157c72e12&amp;id=7a595cd258&c=?', // the url where we want to POST
-        data        : formData, // our data object
-        dataType    : 'jsonp', // what type of data do we expect back from the server
-        encode          : true,
-        error       : function(err) {
-          alert("Could not connect to the registration server. Please try again later.");
-        },
-        success     : function(data) {
-            if (data.result != "success") {
-                // Something went wrong, do something to notify the user. maybe
-                alert(data.msg);
-            } else {
-                // It worked, carry on...
-                alert("success");
-            }
-        }
+      return true;
+  }
+
+  function isValidName($form) {
+      // If email is empty, show error message.
+      // contains just one @
+      var name = $form.find("#name").val();
+      console.log(name);
+      if (!name || !name.length) {
+          return false;
+      }
+
+      return true;
+  }
+
+  function ajaxMailChimpForm($form, $resultElement, $resultDescription){
+      // Hijack the submission. We'll submit the form manually.
+      $form.submit(function(e) {
+          e.preventDefault();
+          $('.flipper').css("transform", "rotateY(180deg)");
+          if (!isValidEmail($form)) {
+              var error =  "Don't missed out your email";
+              var error_text =  "We promise we never spam.";
+              $resultElement.html(error);
+              $resultDescription.html(error_text);
+              $(".return-btn").show();
+              $(".close-btn").hide();
+          } else if (!isValidName($form)) {
+            var error =  "Something's wrong.";
+            var error_text =  "Arh..Seem like you forgot to insert your beautiful name.";
+              $resultElement.html(error);
+              $resultDescription.html(error_text);
+              $(".return-btn").show();
+              $(".close-btn").hide();
+          } else {
+              $('.flipper').css("transition", "2s");
+              $('.flipper').css("transform", "rotateY(1260deg)");
+              $resultElement.html("Subscribing...");
+              $resultDescription.html("A little bit of patience, please");
+              submitSubscribeForm($form, $resultElement, $resultDescription);
+              $(".close-btn").show();
+              $(".return-btn").hide();
+          }
       });
-      event.preventDefault();
-    });
-  });
+  }
+
+  function submitSubscribeForm($form, $resultElement, $resultDescription) {
+      $.ajax({
+          type: "GET",
+          url: $form.attr("action"),
+          data: $form.serialize(),
+          cache: false,
+          dataType: "jsonp",
+          jsonp: "c",
+          contentType: "application/json; charset=utf-8",
+
+          error: function(error){
+              // According to jquery docs, this is never called for cross-domain JSONP requests
+          },
+
+          success: function(data){
+              if (data.result != "success") {
+                  var message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
+
+                  if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
+                      message = "You already in the list";
+                      message_text = "You're subscribed. Thank you."
+                  }
+                  $resultElement.html(message);
+                  $resultDescription.html(message_text);
+              } else {
+                  $resultElement.html("Yeah. Got it. One more step!");
+                  $resultDescription.html("Thank you!<br>You must confirm the subscription in your inbox.");
+              }
+          }
+      });
+  }
+
+  ajaxMailChimpForm($("#standard-subscribe-form"), $("#thank-you-title"));
+
+  // $(".trial-pack-validate .subscribe-button").click(function() {
+  //   $('.trial-pack-validate').submit(function(event) {
+  //     var formData = {
+  //     'mce-NAME' : $('.trial-pack-validate #mce-NAME').val(),
+  //     'mce-EMAIL' : $('.trial-pack-validate #mce-EMAIL').val(),
+  //     'mce-MMERGE3' : $('.trial-pack-validate #mce-MMERGE3').val()
+  //     };
+
+  //     console.log(formData);
+
+  //     $.ajax({
+  //       type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+  //       url         : 'https://github.us14.list-manage.com/subscribe/post?u=3e394ca01b95d97f157c72e12&amp;id=7a595cd258&c=?', // the url where we want to POST
+  //       data        : formData, // our data object
+  //       dataType    : 'jsonp', // what type of data do we expect back from the server
+  //       encode          : true,
+  //       error       : function(err) {
+  //         alert("Could not connect to the registration server. Please try again later.");
+  //       },
+  //       success     : function(data) {
+  //           if (data.result != "success") {
+  //               // Something went wrong, do something to notify the user. maybe
+  //               alert(data.msg);
+  //           } else {
+  //               // It worked, carry on...
+  //               alert("success");
+  //           }
+  //       }
+  //     });
+  //     event.preventDefault();
+  //   });
+  // });
 
   $(".work-btn").click(function() {
     $('html, body').animate({
@@ -96,6 +189,10 @@ $(document).ready(function() {
     $('.flipper').css("transform", "rotateY(0deg)");
     $(".popup-background").css("display", "block");
     $(".popup-background").hide();
+  });
+
+  $(".return-btn").click(function() {
+    $('.flipper').css("transform", "rotateY(0deg)");
   });
 
   $(window).scroll(function () {
